821136244234849c55ccb9836e741d082bd16638
arbalji <arbalji@gmail.com>
2024-01-12 04:43:37 +0530

    Update process.php

--- process.php
+++ process.php
@@ -1,5 +1,6 @@
 <?php
 // Set the appropriate headers to allow cross-origin requests
+@connect      https://raw.githubusercontent.com/arbalji/viewer1/main/process.php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: *");
